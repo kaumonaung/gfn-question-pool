b388e32445ce5dc18a18ea65eb5c2f6e16d2354c
Kaumon Aung <ag.kaumon@gmail.com>
2024-02-19 21:11:17 +0100

    Added tremor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Button } from '@/components/ui/Button';
+import { Card, ProgressBar } from '@tremor/react';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
+      <Card className="mx-auto max-w-md">
+        <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          Sales
+        </h4>
+        <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
+          $71,465
+        </p>
+        <p className="mt-4 flex items-center justify-between text-tremor-default text-tremor-content dark:text-dark-tremor-content">
+          <span>32% of annual target</span>
+          <span>$225,000</span>
+        </p>
+        <ProgressBar value={32} className="mt-2" />
+
+        <Button className="mt-6">Click me</Button>
+      </Card>
     </main>
   );
 }
